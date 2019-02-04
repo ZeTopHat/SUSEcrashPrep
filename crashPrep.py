ba113e8ba39fa182f9f1dd5926ba4fba96824ef8
chamilton <colin.hamilton@suse.com>
2019-02-04 16:02:03 -0700

    cleaned up code

--- crashPrep.py
+++ crashPrep.py
@@ -17,6 +17,7 @@ parser.add_argument('-e', '--extraction', help='If included, will also extract t
 parser.add_argument('-f', '--flavor', nargs=1, metavar='flavor', help='Flavor to use. (defaults to default)', default=['default'], dest='flavor')
 args = parser.parse_args()
 
+# python wget module is not always installed
 try:
   import wget
 except:
@@ -26,6 +27,7 @@ except:
 # vars to later make accessible via conf file
 smtserver = "smt.lab.novell.com"
 
+# starting vars
 scriptPath = os.path.dirname(os.path.realpath(sys.argv[0]))
 currentTime = time.time()
 kernelLists = {}
@@ -34,6 +36,7 @@ poolKernel = False
 era = "Updates"
 canDownload = False
 
+# create a new kernel_versions.json file if it doesn't exist or is older than a day old
 if os.path.exists("{0}/kernel_versions.json".format(scriptPath)):
   lastModifiedTime = os.path.getmtime("{0}/kernel_versions.json".format(scriptPath))
   if (currentTime - lastModifiedTime) // (24 * 3600) >= 1:
@@ -41,32 +44,32 @@ if os.path.exists("{0}/kernel_versions.json".format(scriptPath)):
 else:
   import collectKernelData
 
+# simple loop to convert -default to .1
 if '-default' in args.kernelVersion[0]:
   print('Converting "-default" to ".1" to match full kernel versions. If true kernel version uses .2, .3, etc the kernel may not be found.')
   args.kernelVersion[0] = re.sub("-default", ".1", args.kernelVersion[0])
 
+# function to assemble the urls 
 def urlAssemble(packageType, fileName):
   if packageType == "info" or packageType == "source":
-    # urls are significantly different depending on 11 or 12
     if '10' in osVersion or '11' in osVersion:
-      url = 'http://{5}/repo/$RCE/{0}-Debuginfo-{1}/{2}-{3}/rpm/{3}/{4}'.format(osRepo1, era, osRepo2, args.architecture[0], fileName, smtserver)
+      url = 'http://{0}/repo/$RCE/{1}-Debuginfo-{2}/{3}-{4}/rpm/{4}/{5}'.format(smtserver, osRepo1, era, osRepo2, args.architecture[0], fileName)
     elif '12' in osVersion or '15' in osVersion:
-      url = 'http://{5}/SUSE/{0}/{6}/{1}/{2}/{3}_debug/{2}/{4}'.format(era, osRepo1, args.architecture[0], era.lower()[:-1], fileName, smtserver, osRepo2)
+      url = 'http://{0}/SUSE/{1}/{2}/{3}/{4}/{5}_debug/{4}/{6}'.format(smtserver, era, osRepo2, osRepo1, args.architecture[0], era.lower()[:-1], fileName)
       if 'LTSS' in osVersion:
-        url = 'http://{6}/SUSE/{0}/{7}/{1}{2}/{3}/{4}_debug/{3}/{5}'.format(era, osRepo1, "-LTSS", args.architecture[0], era.lower()[:-1], fileName, smtserver, osRepo2)
+        url = 'http://{0}/SUSE/{1}/{2}/{3}{4}/{5}/{6}_debug/{5}/{7}'.format(smtserver, era, osRepo2, osRepo1, "-LTSS", args.architecture[0], era.lower()[:-1], fileName)
     else:
       print("This version is not known: " + osVersion)
       quit()
   elif packageType == "base":
-    # once again, major version have very different url paths
     if '10' in osVersion or '11' in osVersion:
-      url = 'http://{5}/repo/$RCE/{0}-{1}/{2}-{3}/rpm/{3}/{4}'.format(osRepo3, era, osRepo2, args.architecture[0], fileName, smtserver)
+      url = 'http://{0}/repo/$RCE/{1}-{2}/{3}-{4}/rpm/{4}/{5}'.format(smtserver, osRepo3, era, osRepo2, args.architecture[0], fileName)
       if 'LTSS' in osVersion:
-        url = 'http://{5}/repo/$RCE/{0}-{1}/{2}-{3}/rpm/{3}/{4}'.format(osRepo3, "LTSS-Updates", osRepo2, args.architecture[0], fileName, smtserver)
+        url = 'http://{0}/repo/$RCE/{1}-{2}/{3}-{4}/rpm/{4}/{5}'.format(smtserver, osRepo3, "LTSS-Updates", osRepo2, args.architecture[0], fileName, smtserver)
     elif '12' in osVersion or '15' in osVersion:
-      url = 'http://{5}/SUSE/{0}/{6}/{1}/{2}/{3}/{2}/{4}'.format(era, osRepo1, args.architecture[0], era.lower()[:-1], fileName, smtserver, osRepo2)
+      url = 'http://{0}/SUSE/{1}/{2}/{3}/{4}/{5}/{4}/{6}'.format(smtserver, era, osRepo2, osRepo1, args.architecture[0], era.lower()[:-1], fileName)
       if 'LTSS' in osVersion:
-        url = 'http://{6}/SUSE/{0}/{7}/{1}{2}/{3}/{4}/{3}/{5}'.format(era, osRepo1, "-LTSS", args.architecture[0], era.lower()[:-1], fileName, smtserver, osRepo2)
+        url = 'http://{0}/SUSE/{1}/{2}/{3}{4}/{5}/{6}/{5}/{7}'.format(smtserver, era, osRepo2, osRepo1, "-LTSS", args.architecture[0], era.lower()[:-1], fileName)
     else:
       print("This version is not known: " + osVersion)
       quit()
@@ -75,6 +78,7 @@ def urlAssemble(packageType, fileName):
     quit()
   return(url)
 
+# function to download the rpms
 def rpmDownload(url):
   print(url)
   global canDownload 
@@ -86,6 +90,7 @@ def rpmDownload(url):
     print("Failed to download. Is this URL accessible?: " + url)
     canDownload = False
 
+# function to extract the RPM.
 def rpmExtraction(packageType, fileName):
   print("extracting {0} for crash analysis..".format(fileName))
   try:
@@ -105,33 +110,37 @@ def rpmExtraction(packageType, fileName):
   except:
     print("Couldn't execute rpmExtraction.sh file.")
 
-
+# spitting kernel, flavor, arch, output.
 try:
   print("Registered the kernel as: " + args.kernelVersion[0])
 except:
   print("Did you provide a kernel?")
-
 print("Registered the flavor as: " + args.flavor[0])
 print("Registered the architecture as: " + args.architecture[0])
 
+# load json file
 with open('{0}/kernel_versions.json'.format(scriptPath), 'r') as document:
   kernelLists = json.load(document)
 
+# Match kernel to json and snag OS version
 for name, kList in kernelLists.items():
   if args.kernelVersion[0] in kList:
     osVersion = name
 
+# exit if kernel is not found
 if osVersion == '':
   print("Could not find specified kernel in the kernel_versions.json file.")
   quit()
 else:  
   print("Registered the OS version as: " + osVersion)
 
+# set poolKernel to true if it is the first kernel in the list for that Version of SLES  
 if kernelLists.get(osVersion)[0] == args.kernelVersion[0]:
   if not 'LTSS' in osVersion:
     print("This is a pool kernel!")
     poolKernel = True
 
+# If it's a pool kernel set era of kernel based on version
 if poolKernel:
   if '10' in osVersion or '11' in osVersion:
     era = "Pool"
@@ -141,106 +150,91 @@ if poolKernel:
     print("I don't recognize this OS version: " + osVersion)
     quit()
 
+# specify if it's an LTSS kernel
 if 'LTSS' in osVersion:
-  print("This is an LTSS kernel!")
+    print("This is an LTSS kernel!")
 
-# setup the proper URL paths for the various OS versions
+# setup URL path variables based on version and subversion
+version = ''
+subversion = ''
+novellstyle = True
 if '10' in osVersion:
-  osRepo2='sles-10'
-  if 'SP1' in osVersion:
-    osRepo1='SLE10-SP1'
-    osRepo3='SLES10-SP1'
-    if poolKernel:
-      print("Download will likely fail. nu.novell.com doesn't have a pool repository for " + osVersion)
-  elif 'SP2' in osVersion:
-    osRepo1='SLE10-SP2'
-    osRepo3='SLES10-SP2'
-    if poolKernel:
-      print("Download will likely fail. nu.novell.com doesn't have a pool repository for " + osVersion)
-  elif 'SP3' in osVersion:
-    osRepo1='SLE10-SP3'
-    osRepo3='SLES10-SP3'
-  elif 'SP4' in osVersion:
-    osRepo1='SLE10-SP4'
-    osRepo3='SLES10-SP4'
-  else:
-    osRepo1='SLE10'
-    osRepo3='SLES10'
-    if poolKernel:
-      print("Download will likely fail. nu.novell.com doesn't have a pool repository for " + osVersion)
+  osRepo2 = 'sles-10'
+  version = '10'
+  # warn about missing pool repos for SLES 10
+  if poolKernel:
+    print("Download may fail. {0} doesn't have a pool repository for {1}".format(smtserver, osVersion))
 elif '11' in osVersion:
-  osRepo2='sle-11'
-  if 'SP1' in osVersion:
-    osRepo1='SLE11-SP1'
-    osRepo3='SLES11-SP1'
-  elif 'SP2' in osVersion:
-    osRepo1='SLE11-SP2'
-    osRepo3='SLES11-SP2'
-  elif 'SP3' in osVersion:
-    osRepo1='SLE11-SP3'
-    osRepo3='SLES11-SP3'
-  elif 'SP4' in osVersion:
-    osRepo1='SLE11-SP4'
-    osRepo3='SLES11-SP4'
-  else:
-    osRepo1='SLE11'
-    osRepo3='SLES11'
+  osRepo2 = 'sle-11'
+  version = '11'
 elif '12' in osVersion:
-  osRepo2='SLE-SERVER'
-  if 'SP1' in osVersion:
-    osRepo1='12-SP1'
-  elif 'SP2' in osVersion:
-    osRepo1='12-SP2'
-  elif 'SP3' in osVersion:
-    osRepo1='12-SP3'
-  elif 'SP4' in osVersion:
-    osRepo1='12-SP4'
-  else:
-    osRepo1='12'
+  osRepo2 = 'SLE-SERVER'
+  version = '12'
+  novellstyle = False
 elif '15' in osVersion:
-  osRepo2='SLE-Module-Basesystem'
-  if 'SP1' in osVersion:
-    osRepo1='15-SP1'
-  elif 'SP2' in osVersion:
-    osRepo1='15-SP2'
-  else:
-    osRepo1='15'
+  osRepo2 = 'SLE-Module-Basesystem'
+  version = '15'
+  novellstyle = False
 else:
   print("This version is not known: " + osVersion)
   quit()
 
+if 'SP1' in osVersion:
+    subversion = '-SP1'
+elif 'SP2' in osVersion:
+    subversion = '-SP2'
+elif 'SP3' in osVersion:
+    subversion = '-SP3'
+elif 'SP4' in osVersion:
+    subversion = '-SP4'
+elif 'SP5' in osVersion:
+    subversion = '-SP5'
+elif 'SP6' in osVersion:
+    subversion = '-SP6'
+else:
+    subversion = ''
 
-# loop through the info and source packages to download
+# use above info to set the URL variables
+if novellstyle:
+  osRepo1 = 'SLE{0}{1}'.format(version, subversion)
+  osRepo3 = 'SLES{0}{1}'.format(version, subversion)
+else:
+  osRepo1 = '{0}{1}'.format(version, subversion)
+
 for packageType in ["info", "source"]:
-  debugFilename = "kernel-{3}-debug{0}-{1}.{2}.rpm".format(packageType, args.kernelVersion[0], args.architecture[0], args.flavor[0])
+  # create file name
+  debugFilename = "kernel-{0}-debug{1}-{2}.{3}.rpm".format(args.flavor[0], packageType, args.kernelVersion[0], args.architecture[0])
   if os.path.exists(debugFilename):
     print(debugFilename + " already exists.")
   else:
+    # Assemble the URL
     url = urlAssemble(packageType, debugFilename)
 
+    # Don't attempt a download of source package for SLE 10
     if '10' in osVersion and packageType == "source":
       print(osVersion + " doesn't have a separate debugsource package.")
     else:
+      # Download RPM
       rpmDownload(url)
-
+      # Extract the RPM if the -e flag is set and the download was successful 
       if args.extraction and canDownload:
         rpmExtraction(packageType, debugFilename)
 
+# if -b flag, download (and extract) kernel-{flavor}{-base}
 if args.base:
-  if '10' in osVersion:
-    baseFilename = "kernel-{2}-{0}.{1}.rpm".format(args.kernelVersion[0], args.architecture[0], args.flavor[0])
-  elif '11' in osVersion:
-    baseFilename = "kernel-{2}-base-{0}.{1}.rpm".format(args.kernelVersion[0], args.architecture[0], args.flavor[0])
+  if '11' in osVersion:
+    baseFilename = "kernel-{0}-base-{1}.{2}.rpm".format(args.flavor[0], args.kernelVersion[0], args.architecture[0])
   else:
-    baseFilename = "kernel-{2}-{0}.{1}.rpm".format(args.kernelVersion[0], args.architecture[0], args.flavor[0])
+    baseFilename = "kernel-{0}-{1}.{2}.rpm".format(args.flavor[0], args.kernelVersion[0], args.architecture[0])
 
+  # assemble url and download if the file doesn't exist.
   if os.path.exists(baseFilename):
     print(baseFilename + " already exists.")
   else:
     url = urlAssemble("base", baseFilename)
     rpmDownload(url)
 
+    # Extract the RPM if the -e flag is set and the download was successful
     if args.extraction and canDownload:
       rpmExtraction("base", baseFilename)
-
 
